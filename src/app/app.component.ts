5f82ba61a8dff338a5425c05ed0111da52b7906f
ujwal <patilujwal1997@gmail.com>
2020-03-25 20:27:50 +0530

    file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'regis';
+ name:string=null;
+ email:string=null;
+ password:string=null;
+ city:string=null;
+ country:string=null;
+ userInputData="vaibhav";
+ childdata;
+
+ myStyle={'color':'white','background-color':'grey','width':'250px','height':'150px','position':'relative'};
+ 
+ isTextdisplay=true;
+ isTextdisplay1=true;
+ isTextdisplay2=true;
+
+ displayText(event){
+  this.isTextdisplay = false;
+  }
+ displaydata(){
+this.isTextdisplay1 = false;
+ }
+
+ displayInputBox(){
+  this.isTextdisplay2=false;
+ }
 }
